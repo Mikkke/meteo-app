9a16f72484ff643c3fe160fe8ebc98ca9d778b54
Mikkke <jkellymichael@gmail.com>
2020-07-21 14:11:53 +0200

    Updates

---
+++ precache-manifest.217b2fdb3265573308c0d352b612d7ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c98af2486359521f9e2df52b60b6c9",
+    "url": "/meteo-app/index.html"
+  },
+  {
+    "revision": "35bb0029162f5fe67a72",
+    "url": "/meteo-app/static/css/main.71624e34.chunk.css"
+  },
+  {
+    "revision": "af68144e422f0fc95a16",
+    "url": "/meteo-app/static/js/2.9fa2a681.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meteo-app/static/js/2.9fa2a681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35bb0029162f5fe67a72",
+    "url": "/meteo-app/static/js/main.60e05a33.chunk.js"
+  },
+  {
+    "revision": "45961f93c3b4c30dc194",
+    "url": "/meteo-app/static/js/runtime-main.53e68996.js"
+  }
+]);
